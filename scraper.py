066efca4a443e86618fc5bc44a566bdf7f48021e
tippenein <tippenein@gmail.com>
2013-01-30 09:50:02 -0600

    differentiate west and east bank

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-url = "http://wvprd.ocm.umn.edu/gpcwv/wv3_servlet/urd/run/wv_space.DayList?spfilter=947166,spdt=20130130,lbdviewmode=list"
-usock = urllib2.urlopen(url)
+east = "http://wvprd.ocm.umn.edu/gpcwv/wv3_servlet/urd/run/wv_space.DayList?spfilter=947166,spdt=20130130,lbdviewmode=list"
+west = "http://wvprd.ocm.umn.edu/gpcwv/wv3_servlet/urd/run/wv_space.DayList?spfilter=947169,spdt=20130130,lbdviewmode=list"
+usock = urllib2.urlopen(west)
 data = usock.read()
 usock.close()
 
