4e7f462b424069117cd556e0f08d623ecf8c9788
tippenein <tippenein@gmail.com>
2013-01-30 10:13:13 -0600

    use files instead of reading html over socket

--- scraper.py
+++ scraper.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup
 import urllib2
 
+'''
+Don't fetch the html each time, put this in later
+
 east = "http://wvprd.ocm.umn.edu/gpcwv/wv3_servlet/urd/run/wv_space.DayList?spfilter=947166,spdt=20130130,lbdviewmode=list"
 west = "http://wvprd.ocm.umn.edu/gpcwv/wv3_servlet/urd/run/wv_space.DayList?spfilter=947169,spdt=20130130,lbdviewmode=list"
 usock = urllib2.urlopen(west)
 data = usock.read()
 usock.close()
+'''
 
-soup = BeautifulSoup(data)
-for entry in soup.find_all('tr'):
+soup = BeautifulSoup(open('WestBank.html'))
+table = soup.table
+for entry in table.find_all('tr'):
     print entry.prettify()
