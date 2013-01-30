0cb627fefd2d951ecdf1985864ff30f06052b4ca
tippenein <tippenein@gmail.com>
2013-01-30 15:15:59 -0600

    got all correct td elements

--- scraper.py
+++ scraper.py
@@ -1,9 +1,26 @@
+'''
+project: classroom_finder
+file: scraper.py
+
+This is the terrible structure that they use.
+ContentBox
+    tr
+        td.ListText
+            Location
+        td.ListText
+            Start
+        td.ListText
+            End
+        td.ListText
+            Event
+
+'''
+
 from bs4 import BeautifulSoup
 import urllib2
 
 '''
-Don't fetch the html each time, put this in later
-
+#TODO Use this to fetch fresh data
 east = "http://wvprd.ocm.umn.edu/gpcwv/wv3_servlet/urd/run/wv_space.DayList?spfilter=947166,spdt=20130130,lbdviewmode=list"
 west = "http://wvprd.ocm.umn.edu/gpcwv/wv3_servlet/urd/run/wv_space.DayList?spfilter=947169,spdt=20130130,lbdviewmode=list"
 usock = urllib2.urlopen(west)
@@ -13,6 +30,9 @@ usock.close()
 
 soup = BeautifulSoup(open('WestBank.html'))
 content = soup.find('div', {'id': 'ContentBox'})
+stuff = []
 for tr in content.table.find_all('tr'):
-    print tr.prettify()
-    tr.find('td', {'class': 'ListText'})
+    if (tr.find_all('td', {'class': 'ListText'}) ):
+        stuff.append(tr.findAll('td'))
+
+for item in stuff: print item
