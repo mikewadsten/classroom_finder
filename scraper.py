09251e01f4a7782e0d5ed637b18d2cd14ba0259e
tippenein <tippenein@gmail.com>
2013-01-30 21:03:21 -0600

    tidy up. fix times list

--- scraper.py
+++ scraper.py
@@ -30,30 +30,45 @@ data = usock.read()
 usock.close()
 '''
 
-soup = BeautifulSoup(open('WestBank.html'), "html.parser")
-content = soup.find('div', {'id': 'ContentBox'})
-eventList = []
-for tr in content.table.findAll('tr'):
-    if (tr.find_all('td', {'class': 'ListText'}) ):
-        # this is where it grabs the 4 ListText elements
-        # makes a list of lists [[building1, start, end, event],[building2, start, end, event] ... etc]
-        eventList.append(tr.findAll('td'))
 
-events = {}
-for event in eventList:
-    room = event[0].find('a', {'class': 'ListText'}).string
-    print room
-    start_times = event[1].stripped_strings
-    end_times = event[2].stripped_strings
-    for start_time, end_time in zip(start_times, end_times):
-        # Strip colon, AM/PM and spaces
-        f_start_time = start_time.replace(':','').strip(': AMPM')
-        f_end_time = end_time.replace(':','').strip(': AMPM')
-        #Dictionary of events, keyed by room, value is a list of start and end times
-        events[room]=[f_start_time, f_end_time]
-        print f_start_time, f_end_time
-    print "\n"
-    #events[room] = 
-    #events['start'] = 
-    #events['end'] = 
+def init(source):
+    ''' gather html data and generate the event dictionary from it 
+        @param source - string of URL or FILE to grab data from
+    '''
+    if source.endswith('.html'):
+        _file=True
+    if _file:
+        soup = BeautifulSoup(open(source), "html.parser")
+    else:
+        soup = BeautifulSoup(source)
 
+    content = soup.find('div', {'id': 'ContentBox'})
+    eventList = []
+    for tr in content.table.findAll('tr'):
+        if (tr.find_all('td', {'class': 'ListText'}) ):
+            # this is where it grabs the 4 ListText elements
+            # makes a list of lists [[building1, start, end, event],[building2, start, end, event] ... etc]
+            eventList.append(tr.findAll('td'))
+
+    events = {}
+    for event in eventList:
+        room = event[0].find('a', {'class': 'ListText'}).string
+        start_times = event[1].stripped_strings
+        end_times = event[2].stripped_strings
+        times = []
+        for start_time, end_time in zip(start_times, end_times):
+            # Strip colon, AM/PM and spaces
+            f_start_time = start_time.replace(':','').strip(': AMPM')
+            f_end_time = end_time.replace(':','').strip(': AMPM')
+            #events dict, key=room, value=[start time, end time]
+            times.append([f_start_time, f_end_time])
+        events[room] = times
+
+    return events
+
+def get_gaps(events):
+    pass
+
+if __name__ == '__main__':
+    events = init('EastBank.html')
+    print events['FOLH000012']
