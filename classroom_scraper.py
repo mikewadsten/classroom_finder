1aa31b099d4e744c340c6c0b9da24ec56800f3bf
AJ Stuyvenberg <stuyv004@umn.edu>
2013-02-06 19:47:48 -0600

    DUPLICATE LAST RESULT - Parsing room name and room ID, all column info commented out.

--- classroom_scraper.py
+++ classroom_scraper.py
@@ -8,6 +8,8 @@ import sqlite3
 import datetime
 import urllib2
 import os
+import re
+
 today = datetime.date.today()
 url = "http://www.classroom.umn.edu/roomsearch/results.php?building=&room_number="\
         "&seating_capacity=&seat_type=&campus=&pagesize=9999&Submit1=Search"
@@ -17,7 +19,7 @@ conn = sqlite3.connect('database.db')
 db = conn.cursor()
 
 def init_db():
-    db.execute('''DROP TABLE IF EXISTS gaps''')
+    db.execute('''DROP TABLE IF EXISTS classrooms''')
     db.execute('''CREATE TABLE classrooms (spaceID INT, roomname STRING, capacity INT)''')
 
 def insert_classroom(spaceID,name,capacity):
@@ -41,11 +43,18 @@ def init():
 
     rows = soup.table.tbody.findAll('tr')
     for row in rows:
-        for column in row:
-            print "~~~~~~~~~~~~~~~COLUMN INFO~~~~~~~~~~~~~~~"
-            print column
+        #for column in row:
+            #print "~~~~~~~~~~~~~~~COLUMN INFO~~~~~~~~~~~~~~~"
+            #print column
         print "~~~~~~~~~~~~~~~ROW~~~~~~~~~~~~~~~~~~~~~`"
-        print row
+        classroom_search = row.find('a', {'class', 'roomInfo'})
+        if classroom_search:
+            classroom_name = classroom_search.get_text()
+        url_search = row.find('a', {'class', 'roomInfo'})
+        if url_search:
+            classroom_id = re.search('(?<=RoomID=)\w+', url_search.get('href')).group(0)
+        print classroom_name
+        print classroom_id
 
 def construct_url(sid):
     ''' this is the individual room page. We may or may not need it'''
