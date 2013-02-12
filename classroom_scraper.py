2293ee3098609e2ab664baaefc950e25a5850ee2
AJ Stuyvenberg <stuyv004@umn.edu>
2013-02-11 23:21:49 -0600

    adding classroom scraper to DB, fixing wierd TD tags in dump_classrooms.html lines 2026, 6653, and 10636. Seems fine in current classroom.umn.edu code.

--- classroom_scraper.py
+++ classroom_scraper.py
@@ -14,17 +14,18 @@ today = datetime.date.today()
 url = "http://www.classroom.umn.edu/roomsearch/results.php?building=&room_number="\
         "&seating_capacity=&seat_type=&campus=&pagesize=9999&Submit1=Search"
 
+classrooms = {}
+feature_headers = ['proj','dvd','vcr','doc','chalk','marker','alc']
 
 conn = sqlite3.connect('database.db')
 db = conn.cursor()
 
 def init_db():
     db.execute('''DROP TABLE IF EXISTS classrooms''')
-    db.execute('''CREATE TABLE classrooms (spaceID INT, roomname STRING, capacity INT)''')
+    db.execute('''CREATE TABLE classrooms (spaceID INT, roomname STRING, capacity INT, seat_type STRING, projector STRING, dvd STRING, vcr STRING, doc STRING, chalk STRING, marker STRING, alc STRING)''')
 
-def insert_classroom(spaceID,name,capacity):
-    query = "INSERT INTO classrooms VALUES ({},\"{}\",{})".format(
-            spaceID, name, capacity)
+def insert_classroom(spaceID,name,capacity,seat_type,proj,dvd,vcr,doc,chalk,marker,alc):
+    query = "INSERT INTO classrooms VALUES (\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\")".format(spaceID,name,capacity,seat_type,proj,dvd,vcr,doc,chalk,marker,alc)
     db.execute(query)
 
 def init():
@@ -44,20 +45,39 @@ def init():
         soup = BeautifulSoup(open("test/dump_classrooms.html"), parser)
 
     rows = soup.table.tbody.findAll('tr', class_=re.compile("even_row|odd_row"))
-    # dirty hack, we seem to get a duplicate final row
     for row in rows:
+        print "~~~~~~~~~~~~~~~~ NEW CLASSROOM ~~~~~~~~~~~~~~~~"
         try:
-            classroom_name = row.find('a', {'class', 'roomInfo'}).get_text()
             url_search = row.find('a', {'class', 'roomInfo'})
             classroom_id = re.search('(?<=RoomID=)\w+', url_search.get('href')).group(0)
+            features = row.find_all('td')
         #TODO get correct exceptions and raise
         except:
             raise ScrapeError("search for html data failed")
-        insert_classroom(classroom_id, classroom_name, 1)
+
+        classroom_name = features[0].get_text()
+        classroom_capacity = features[1].get_text()
+        classroom_seat_type = features[2].get_text()
+        classroom_proj = hasFeature(features[3])
+        classroom_dvd = hasFeature(features[4])
+        classroom_vcr = hasFeature(features[5])
+        classroom_doc = hasFeature(features[6])
+        classroom_chalk = hasFeature(features[7])
+        classroom_marker = hasFeature(features[8])
+        classroom_alc = hasFeature(features[9])
+
+
+        insert_classroom(classroom_id, classroom_name, classroom_capacity, classroom_seat_type, classroom_proj, classroom_dvd, classroom_vcr, classroom_doc, classroom_chalk, classroom_marker, classroom_alc)
     conn.commit()
 
 class ScrapeError(Exception):
     pass
+
+def hasFeature(feature):
+    print feature
+    if feature.get('class')[0]=='checkmark':
+        return "yes"
+    return "no"
 
 def construct_url(sid):
     ''' this is the individual room page. We may or may not need it'''
