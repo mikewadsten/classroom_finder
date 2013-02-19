255ca09c295348a1fbb6c62680c8aeadeb2e88a1
tippenein <tippenein@gmail.com>
2013-02-18 21:02:56 -0600

    unittests

--- test_app.py
+++ test_app.py
@@ -5,22 +5,21 @@ import unittest
 
 class AppTestCase(unittest.TestCase):
 
-    def setup(self):
+    def setUp(self):
         ''' setup a temp database'''
         self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
         self.app = app.app.test_client()
-        app.app.connect_db()
 
     def tearDown(self):
         '''Get rid of the database again after each test.'''
-        try:
-            os.close(self.db_fd)
-            os.unlink(app.app.config['DATABASE'])
-        except:
-            pass
+        os.close(self.db_fd)
+        os.unlink(app.app.config['DATABASE'])
 
     def test_urls(self):
         r = self.app.get('/')
+        self.assertEquals(r.status_code, 200)
+        r = self.app.get('/now')
         self.assertEquals(r.status_code, 200)
 
 if __name__ == '__main__':
